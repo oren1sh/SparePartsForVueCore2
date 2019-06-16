fc5df71fe5f6ca35e4ef012e42a968d0ee0287ae
Oren <oren1shalev@gmail.com>
2019-06-16 14:26:57 +0300

    upload the static files

---
+++ CarTestCoreReVue/ClientApp/dist/precache-manifest.9a4c8ff030ea1d942add65b141111d32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b2c73308dcc5d4d53597",
+    "url": "/js/chunk-vendors.cbd56be6.js"
+  },
+  {
+    "revision": "37ea517725c8b67431a0",
+    "url": "/js/app.b7c141d3.js"
+  },
+  {
+    "revision": "b16c4d954912fc10adea91d3e14259ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "b2c73308dcc5d4d53597",
+    "url": "/css/chunk-vendors.14bfa1a8.css"
+  }
+];
